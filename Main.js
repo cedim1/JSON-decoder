e181be1799328b8b22fa445e268951504aa4d464
cedim1 <164538920+cedim1@users.noreply.github.com>
2024-04-08 17:21:15 +0100

    Create Main.js
    Very simple decoder V1.0

---
+++ Main.js
@@ -0,0 +1,10 @@
+function Decode(string, start) {
+  let howlong = string.length;
+  let chars = string;
+  var done = start;
+  do {
+  var projid = projid + chars.charAt(done);
+  var done = done + 1;
+  } while(chars.charAt(done) != "}")
+}
+Decode({"id":996391847}, 7)
